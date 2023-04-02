5d92f2f91f2bc9cdf6c86958b004e9fc44805560
ABDULLAHI OMOWUMI IBRAHEEM <71957195+mojicsibro2@users.noreply.github.com>
2023-04-01 22:49:55 -0700

    Add files via upload

---
+++ boxAndCircle portfolio website/index.js
@@ -0,0 +1,60 @@
+
+
+
+function about(){
+ document.getElementById('c6').style.backgroundColor = 'red' ;
+ document.getElementById('ii').style.display = 'none';
+ document.getElementById('im').style.display = 'block';
+ document.getElementById('mm').style.display = 'flex';
+ document.getElementById('pp').style.display = 'none' ;
+ document.getElementById('gg').style.display = 'none';
+ document.getElementById('ww').style.display = 'none';
+ document.getElementById('ll').style.display = 'none';
+
+}
+
+function python(){
+    document.getElementById('c2').style.backgroundColor = 'red' ;
+    document.getElementById('pp').style.display = 'flex';
+    document.getElementById('ii').style.display = 'none' ;
+    document.getElementById('im').style.display = 'none';
+    document.getElementById('mm').style.display = 'none';
+    document.getElementById('gg').style.display = 'none';
+    document.getElementById('ww').style.display = 'none';
+    document.getElementById('ll').style.display = 'none';
+}
+
+function graphic(){
+    document.getElementById('c5').style.backgroundColor = 'red' ;
+    document.getElementById('gg').style.display = 'flex';
+    document.getElementById('pp').style.display = 'none';
+    document.getElementById('ii').style.display = 'none' ;
+    document.getElementById('im').style.display = 'none';
+    document.getElementById('mm').style.display = 'none';
+    document.getElementById('ww').style.display = 'none';
+    document.getElementById('ll').style.display = 'none';
+
+}
+
+function website(){
+    document.getElementById('c1').style.backgroundColor = 'red' ;
+    document.getElementById('ww').style.display = 'flex';
+    document.getElementById('gg').style.display = 'none';
+    document.getElementById('pp').style.display = 'none';
+    document.getElementById('ii').style.display = 'none' ;
+    document.getElementById('im').style.display = 'none';
+    document.getElementById('mm').style.display = 'none';
+    document.getElementById('ll').style.display = 'none';
+
+}
+
+function language(){
+    document.getElementById('c3').style.backgroundColor = 'red' ;
+    document.getElementById('ll').style.display = 'flex';
+    document.getElementById('ww').style.display = 'none';
+    document.getElementById('gg').style.display = 'none';
+    document.getElementById('pp').style.display = 'none';
+    document.getElementById('ii').style.display = 'none' ;
+    document.getElementById('im').style.display = 'none';
+    document.getElementById('mm').style.display = 'none';
+}
